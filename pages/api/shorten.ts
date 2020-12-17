a29ae71bda1fc3e795f12bf338b5a386888aa999
iLuiizUHD <luizgomes749@gmail.com>
2020-12-16 21:17:30 -0300

    Url shortener now works nice

--- pages/api/shorten.ts
+++ pages/api/shorten.ts
@@ -1,22 +1,23 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { nanoid } from 'nanoid';
-import denv from 'dotenv';
 import * as yup from 'yup';
 
 // Express Stuff
+import express from 'express';
+
 import expressRateLimit from 'express-rate-limit';
 import expressSlowDown from 'express-slow-down';
-import Cors from 'cors';
+import helmet from 'helmet';
 
 // My stuff
 import URLSModel from './db/urls-model';
-import Middleware from './middlewares/middleware';
 import Connection from './db/index';
+
+const app = express();
+
+app.use(helmet());
 
 // Connect
 Connection();
-
-denv.config();
 
 function getNewAlias(): string {
   return nanoid(8);
@@ -26,25 +27,18 @@ const schema = yup.object().shape({
   url: yup.string().trim().url().required(),
 });
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<boolean> => {
-  if (req.method !== 'POST') {
-    res.status(404).json({ error: 'Cannot GET on this route!' });
-    res.end();
-    return false;
-  }
+app.use(expressSlowDown({
+  windowMs: 30 * 1000,
+  delayAfter: 1,
+  delayMs: 500,
+}));
 
-  // Execute some middlewares
-  await Middleware(req, res, expressSlowDown({
-    windowMs: 30 * 1000,
-    delayAfter: 1,
-    delayMs: 500,
-  }));
+app.use(expressRateLimit({
+  windowMs: 30 * 1000,
+  max: 1,
+}));
 
-  await Middleware(req, res, expressRateLimit({
-    windowMs: 30 * 1000,
-    max: 1,
-  }));
-
+app.post('/api/shorten', async (req, res) => {
   const { url } = req.body;
 
   // Validate the URL
@@ -54,8 +48,21 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<boolea
   const found = ['tidis.net', 'projectsa.net'].map((a) => url.includes(a));
   if (found.length > 0 && found.includes(true)) {
     res.status(403).json({ error: 'Cannot shorten this URL!' });
-    res.end();
     return false;
+  }
+
+  // Find an existing slug
+  const result = await URLSModel.findOne({
+    a_url: url,
+  }).select('a_alias a_url').exec();
+
+  if (result && result?.a_alias) {
+    res.status(418).json({
+      a_alias: result.a_alias,
+      a_url: result.a_url,
+      success: true,
+    });
+    return true;
   }
 
   // Start register the URL
@@ -64,25 +71,9 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<boolea
     a_alias: '',
   };
 
-  let alias = getNewAlias();
-
   try {
-    // Check if there is any alias
-    const query = URLSModel.find({}).select({ a_alias: alias });
-
-    query.exec((err, someValue) => {
-      if (err) throw new Error(err.message);
-
-      if (someValue?.length > 0) {
-        alias = getNewAlias();
-        return true;
-      }
-
-      return false;
-    });
-
     body.a_url = req.body.url;
-    body.a_alias = alias;
+    body.a_alias = getNewAlias();
 
     const URLS = new URLSModel(body);
 
@@ -97,7 +88,6 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<boolea
       ret.success = true;
 
       res.status(201).json(ret);
-      res.end();
       return true;
     });
 
@@ -106,7 +96,8 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<boolea
     //
   } catch (error) {
     res.status(500).json(error);
-    res.end();
     return false;
   }
-};
+});
+
+export default app;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <p>Fuck me in the Áss</p>
+      <p>Fuck me in the Áss!</p>
     </div>
-  )
+  );
 }
