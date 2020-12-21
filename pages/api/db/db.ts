0a9f5b0385e448505d3d190b57b4d2af891e725c
iLuiizUHD <luizgomes749@gmail.com>
2020-12-21 19:17:25 -0300

    Uncomment db

--- pages/api/db/db.ts
+++ pages/api/db/db.ts
@@ -4,11 +4,11 @@ import denv from 'dotenv';
 denv.config();
 
 export default function () {
-  // mongoose.connect(
-  //   `mongodb+srv://tidis:${process.env.DB_PASS}@shortendb.mnjto.mongodb.net/shorten?retryWrites=true&w=majority`,
-  //   {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //   },
-  // );
+  mongoose.connect(
+    `mongodb+srv://tidis:${process.env.DB_PASS}@shortendb.mnjto.mongodb.net/shorten?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
 }
